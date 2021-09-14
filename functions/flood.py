31bfab6e53978be249789c36e8674e756d3a24f0
vraihxjehxjzhsjd <90188100+vraihxjehxjzhsjd@users.noreply.github.com>
2021-09-14 13:32:23 +0300

    Update flood.py

--- functions/flood.py
+++ functions/flood.py
@@ -1,5 +1,6 @@
 import pyrogram, random
-
+global text
+text = ["ты обезьяна", "твою мамашу поебем", "хуяку на", "пососи хуй"]
 @pyrogram.Client.on_message()
 async def ok(client, message):
     count = 0
@@ -7,6 +8,6 @@ async def ok(client, message):
     if message.text != trigger:
         return
     for _ in range(100):
-        await client.send_message(message.chat.id, random.choice(message) + str(count))
+        await client.send_message(message.chat.id, random.choice(text) + str(count))
         print(f"sended. COUNT: {count}")
  
