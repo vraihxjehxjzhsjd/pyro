f212ba454decfa57a9b510fc5e56ab5ddc7de324
vraihxjehxjzhsjd <90188100+vraihxjehxjzhsjd@users.noreply.github.com>
2021-09-14 13:31:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import pyrogram
 
-with open("message.txt", "r+") as file:
-    message = file.read().split("\n")
-    file.close()
-
 pyrogram.Client("1").start()
 print("log as 1")
 pyrogram.Client("2").start()
