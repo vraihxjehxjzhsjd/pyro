4865d37f151d7436d6896e6554e47586f4a10d76
vraihxjehxjzhsjd <90188100+vraihxjehxjzhsjd@users.noreply.github.com>
2021-09-14 13:25:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pyrogram
 
 global count, trigger, message
 trigger = "а"
+count = 0
 with open("message.txt", "r+") as file:
     message = file.read().split("\n")
     file.close()
@@ -10,4 +11,5 @@ pyrogram.Client("1").start()
 print("log as 1")
 pyrogram.Client("2").start()
 print("log as 2")
+print("send 'а' to chat")
 pyrogram.idle()
